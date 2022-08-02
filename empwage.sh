5b7e22f10ce49c674b28d16520d50c04c9e3fba2
aytech11 <aytech0110@gmail.com>
2022-08-02 07:35:14 +0530

    employee check present or absent

--- empwage.sh
+++ empwage.sh
@@ -5,3 +5,15 @@ echo "Welcome to Employee Wage Program"
 echo "UC1 created"
 
 
+isPresent=1
+randomcheck=$((RANDOM%2))
+
+if [ $isPresent -eq $randomcheck ]
+then
+echo "employee is present"
+
+else
+echo "employee is absent"
+
+fi
+
